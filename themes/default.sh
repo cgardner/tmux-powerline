134b136a9c8194106f10388b38afdc27174e126a
Craig Gardner <craig.s.gardner@gmail.com>
2013-01-28 15:17:42 -0800

    Template Customizations

--- themes/default.sh
+++ themes/default.sh
@@ -40,9 +40,9 @@ fi
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
-		"pwd 89 211" \
+		#"pwd 89 211" \
 		"mailcount 9 255" \
-		"now_playing 234 37" \
+		#"now_playing 234 37" \
 		#"cpu 240 136" \
 		"load 237 167" \
 		#"tmux_mem_cpu_load 234 136" \
